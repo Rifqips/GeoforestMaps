ae6daea9fdb0bca371d0ad7e2645263ba54e6ff9
rifqi.siliwangi <rifqipadisiliwangi@gmail.com>
2024-07-12 08:30:25 +0700

    pull project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.window)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/id/application/geoforestmaps/presentation/feature/login/LoginFragment.kt
+++ app/src/main/java/id/application/geoforestmaps/presentation/feature/login/LoginFragment.kt
@@ -1,6 +1,8 @@
 package id.application.geoforestmaps.presentation.feature.login
 
+import android.util.Log
 import androidx.navigation.fragment.findNavController
+import androidx.window.layout.WindowMetricsCalculator
 import id.application.core.utils.BaseFragment
 import id.application.geoforestmaps.R
 import id.application.geoforestmaps.databinding.FragmentLoginBinding
@@ -12,7 +14,32 @@ class LoginFragment :
 
     override val viewModel: VmPreLogin by viewModel()
 
-    override fun initView() {}
+    override fun initView() {
+        with(binding){
+            val metrics = context?.let {
+                WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(it)
+            }
+            val widthDp = metrics?.bounds?.width()?.div(resources.displayMetrics.density)
+            if (widthDp != null) {
+                when {
+                    widthDp <= 320 -> {
+                        Log.d("DeviceType", "Small phone")
+                    }
+                    widthDp <= 360 -> {
+                        consBgLogin.maxHeight = (600 * resources.displayMetrics.density).toInt()
+                        Log.d("DeviceType", "Medium phone")
+                    }
+                    else -> {
+                        val layoutParams = consBgLogin.layoutParams
+                        layoutParams.height = (750 * resources.displayMetrics.density).toInt()
+                        consBgLogin.layoutParams = layoutParams
+                        Log.d("DeviceType", "Large phone or tablet")
+                    }
+                }
+            }
+        }
+
+    }
 
     override fun initListener() {
         with(binding){
